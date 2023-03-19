c705ba079c14f1b46a0f9675b7b6f321292b53ac
xy-Zee <olayiwolaazeemat@gmail.com>
2023-03-19 16:27:23 +0100

    s3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3statebackend18-03"
+    bucket = "s3statebackend19-03"
     region = "us-east-1"
     key = "global/mystatefile/terraform.tfstate"
     encrypt = true
